69ac92886119054abd15a36ece910a0963121ec1
devolo13 <devolo13@gmail.com>
2023-04-07 10:20:35 -0500

    tweak map

--- python/w1d5.js
+++ python/w1d5.js
@@ -8,10 +8,10 @@ function isPalindrome(str) {
 }
 
 function zipArraysIntoMap(keys, values) {
-  if (keys.length == 0 || values.length == 0) {
-    return '';
-  }
   let map = {};
+  if (keys.length == 0 || values.length == 0) {
+    return map;
+  }
   for (let i = 0; i < keys.length; i++) {
     if (values[i] == undefined) {
       return map;

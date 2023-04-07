a547b7dce9361b77765cba01d4b9b7f62339a1c9
devolo13 <devolo13@gmail.com>
2023-04-07 10:18:20 -0500

    added python w1d5

---
+++ python/w1d5.js
@@ -0,0 +1,34 @@
+function isPalindrome(str) {
+  for (let i = 0; i < str.length / 2; i++) {
+    if (str[i] !== str[str.length - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function zipArraysIntoMap(keys, values) {
+  if (keys.length == 0 || values.length == 0) {
+    return '';
+  }
+  let map = {};
+  for (let i = 0; i < keys.length; i++) {
+    if (values[i] == undefined) {
+      return map;
+    } else {
+      map[keys[i]] = values[i];
+    }
+  }
+  return map;
+}
+
+const keys1 = ['abc', 3, 'yo'];
+const vals1 = [42, 'wassup', true];
+const expected1 = {
+  abc: 42,
+  3: 'wassup',
+  yo: true,
+};
+
+console.log(isPalindrome('Racecar'));
+console.log(zipArraysIntoMap(keys1, vals1));

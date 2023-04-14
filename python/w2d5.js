bee387998534c5bd76081a16e34561723acef4f6
devolo13 <devolo13@gmail.com>
2023-04-14 10:56:02 -0500

    cleaned formatting

--- python/w2d5.js
+++ python/w2d5.js
@@ -14,22 +14,7 @@ const expected14 = 'orldHello W';
 
 const rotateAmnt15 = 13;
 const expected15 = 'ldHello Wor';
-/* 
-Explanation: this is 2 more than the length so it ends up being the same
-as rotating it 2 characters because after rotating every letter it gets back
-to the original position.
-*/
 
-/**
- * Rotates a given string's characters to the right by the given amount,
- * wrapping characters to the beginning.
- * - Time: O(?).
- * - Space: O(?).
- * @param {string} str
- * @param {number} amnt The amount of characters in the given str to rotate to the
- *    right.
- * @returns {string} The string rotated by the given amount.
- */
 function rotateStr(str, amnt) {
   amnt = amnt % str.length;
   str = str.split('');
@@ -50,6 +35,8 @@ function rotateStr2(str, amnt) {
   const rotated = str.slice(-effectiveAmnt) + str.slice(0, -effectiveAmnt);
   return rotated;
 }
+
+
 const strA1 = 'ABCD';
 const strB1 = 'CDAB';
 // Explanation: if you start from A in the 2nd string, the letters are in the same order, just rotated
@@ -65,14 +52,7 @@ const strB3 = 'BCDAB';
 // Explanation: same letters in correct order but there is an extra letter.
 const expected23 = false;
 
-/**
- * Determines whether the second string is a rotated version of the first.
- * - Time: O(?).
- * - Space: O(?).
- * @param {string} s1
- * @param {string} s2
- * @returns {boolean} Whether the second string is a rotated version of the 1st.
- */
+
 function isRotation(s1, s2) {
   if (s1.length != s2.length) {
     return false;

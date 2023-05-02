6487397b9af9975656405376a54ac15928572a08
devolo13 <devolo13@gmail.com>
2023-05-02 10:40:35 -0500

    initial commit

---
+++ mern/w1d2.js
@@ -0,0 +1,33 @@
+// bubbleSort()
+/* 
+    https://www.hackerearth.com/practice/algorithms/sorting/bubble-sort/visualize/
+    Stable sort
+    Time Complexity
+        - Best: O(n) linear when array is already sorted.
+        - Average: O(n^2) quadratic.
+        - Worst: O(n^2) quadratic when the array is reverse sorted.
+    Space: O(1) constant.
+    For review, create a function that uses BubbleSort to sort an unsorted array in-place.
+    For every pair of adjacent indices, swap them if the item on the left is larger than the item on the right until array is fully sorted
+    -> Assume there are no duplicates
+*/
+//             i
+//               j
+const nums1 = [5, 3, 4, 2, 1];
+const nums2 = [9, 2, 5, 6, 4, 3, 7, 10, 1, 8];
+const nums3 = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 1; j < arr.length - i; j++) {
+      if (arr[j - 1] > arr[j]) {
+        let temp = arr[j - 1];
+        arr[j - 1] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
+console.log(bubbleSort(nums2));

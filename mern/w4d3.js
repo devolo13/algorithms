07334fc5b2baf5d775bb6d1fb3cc46ef44fffe78
devolo13 <devolo13@gmail.com>
2023-05-24 10:26:48 -0500

    added second function with one loop

--- mern/w4d3.js
+++ mern/w4d3.js
@@ -86,9 +86,53 @@ function backspaceStringCompare(S, T) {
   return sArr.toString() === tArr.toString();
 }
 
+function backspaceStringCompare2(S, T) {
+  // convert S and T to arrays so they're easier to work with
+  let sArr = [...S];
+  let tArr = [...T];
+  // setup our index variable
+  let i = 0;
+  let j = 0;
+  // loop through our arrays
+  while (i < sArr.length || j < tArr.length) {
+    if (i < sArr.length){
+      if (sArr[i] === '#') {
+        // if we find a # remove it and the character before it
+        sArr.splice(i - 1, 2);
+        // then decrement the index to look at the character that would have been behind the #
+        i--;
+      } else {
+        // if our character isn't a # then ignore it and continue on
+        i++;
+      }
+    }
+    if (j < tArr.length){
+      if (tArr[j] === '#') {
+        // if we find a # remove it and the character before it
+        tArr.splice(j - 1, 2);
+        // then decrement the index to look at the character that would have been behind the #
+        j--;
+      } else {
+        // if our character isn't a # then ignore it and continue on
+        j++;
+      }
+    }
+  }
+  // return true if the arrays are the same, false if they are different
+  // need .toString() because of how js compares arrays
+  return sArr.toString() === tArr.toString();
+}
+
 //---------------------
 
 console.log(backspaceStringCompare(S1, T1));
 console.log(backspaceStringCompare(S2, T2));
 console.log(backspaceStringCompare(S3, T3));
 console.log(backspaceStringCompare(S4, T4));
+
+console.log('');
+
+console.log(backspaceStringCompare2(S1, T1));
+console.log(backspaceStringCompare2(S2, T2));
+console.log(backspaceStringCompare2(S3, T3));
+console.log(backspaceStringCompare2(S4, T4));
